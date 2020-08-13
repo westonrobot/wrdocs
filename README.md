5a37bf2cd8d01e1d8696137c895314a26e7961ce
Ruixiang Du <ruixiang.du@westonrobot.com>
2020-08-13 17:35:19 +0800

    removed theme as submodule

--- README.md
+++ README.md
@@ -1,9 +1,23 @@
 # Weston Robot Documentation Source
 
+## Install sphinx
+
+```
+$ pip install -U Sphinx
+```
+
 ## Setup the development repository
 
 ```
 $ git clone --recursive https://github.com/westonrobot/wrdocs.git
-$ cd build
-$ git clone -b gh-pages https://github.com/westonrobot/wrdocs.git
+$ cd wrdocs/build
+$ git clone -b gh-pages https://github.com/westonrobot/wrdocs.git html
+```
+
+## Build with sphinx docker
+
+Following instructions [here](https://docs.docker.com/engine/install/ubuntu/) to install docker engine first.
+
+```
+$ sudo docker run --rm -v ~/Workspace/weston_robot/devel/wrdocs:/docs sphinxdoc/sphinx make html
 ```
--- source/conf.py
+++ source/conf.py
@@ -15,7 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
+# import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
 
@@ -41,7 +41,7 @@ release = u'V0.1.0'
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
-    "sphinx_rtd_theme",
+    # "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -77,8 +77,8 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+html_theme_path = ["./_themes"]
 html_theme = "sphinx_rtd_theme" #'alabaster'
-html_theme_path = ["./_themes", ]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
